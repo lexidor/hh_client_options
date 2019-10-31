72ef03c73bb5a876784fdb998a681f483bceb8c6
Lexidor Digital <31805625+lexidor@users.noreply.github.com>
2019-10-31 15:08:42 +0100

    `unsafe_rx` sticks

---
+++ bad.hack
@@ -0,0 +1,5 @@
+namespace Lexidor\HHClientOptions;
+
+async function main_bad_hack_async(): Awaitable<void> {
+
+}
--- bad.php
+++ bad.php
@@ -1,1 +1,7 @@
 <?hh
+
+namespace Lexidor\HHClientOptions;
+
+async function main_bad_async(): Awaitable<void> {
+
+}
